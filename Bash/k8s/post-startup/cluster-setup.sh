a50a3f41333255d40b0c03657d2446ec35785e9e
Hein Htet Zaw <maintain1346@gmail.com>
2026-08-03 10:42:30 +0630

    updated cluster-setup log line

--- Bash/k8s/post-startup/cluster-setup.sh
+++ Bash/k8s/post-startup/cluster-setup.sh
@@ -210,8 +210,8 @@ setup_metallb() {
 		kubectl get l2advertisements.metallb.io --no-headers -n metallb-system | grep pool > /dev/null 2>&1 && \
 		log success "Metallb IPAddressPool and L2Advertisement already exists" && return 0
 
-	if [ -z "${METALLB_VIP_RANGE}" ]; then
-		log progress "Using virtual IP range from \$METALLB_VIP_LIST"
+	if ! [ -z "${METALLB_VIP_RANGE+true}" ]; then
+		log progress "Using virtual IP range from \$METALLB_VIP_RANGE"
 	else
 		log progress "Auto selecting virtual IP range from node IPs"
 		IFS="." read -ra IP <<< $(kubectl get nodes --no-headers -o custom-columns="IP:.status.addresses[0].address" | head -n 1)
