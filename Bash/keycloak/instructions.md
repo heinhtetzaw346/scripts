31ef66910c59273b1c6de05ab9c6aa18782fd67a
Hein Htet Zaw <maintain1346@gmail.com>
2026-08-06 01:40:45 +0630

    added ovpnx

---
+++ Bash/keycloak/instructions.md
@@ -0,0 +1,11 @@
+# Keycloak Admin API data query
+
+Please write a bash script to pull data from keycloak (rhbk)
+
+Required DATA: users list and events.
+
+Save both of them as csv files.
+
+You can use the .env file. It contains USERNAME and PASSWORD of admin. (please do not look at it. just use it).
+
+Make the url a variable and an input. Use https://nonprod-oneid.yomabank.org as default.
---
+++ Bash/ovpn/ovpnx.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+
+import() {
+	CONFIG_FILE=$(realpath "$1" 2>/dev/null)
+	CONFIG_NAME="$2"
+	[ -f "${CONFIG_FILE}" ] || { echo "[$CONFIG_FILE] file doesn't exist..."; exit 1; }
+	[ -z "${CONFIG_NAME}" ] && { echo "CONFIG_NAME can't be empty"; exit 1; }
+	openvpn3 config-import --config "$CONFIG_FILE" --name "$CONFIG_NAME" --persistent
+}
+
+list() {
+	OPTION="$1"
+	[ -z "${OPTION}" ] && { openvpn3 configs-list; }
+	[ "$OPTION" = "s" ] || [ "$OPTION" = "session" ] && { openvpn3 sessions-list; }
+}
+
+remove() {
+	REMOVE_CONFIG_NAME="$1"
+	[ -n "${REMOVE_CONFIG_NAME}" ] && grep "$REMOVE_CONFIG_NAME" <<< $(list) > /dev/null || { echo "The connection [$REMOVE_CONFIG_NAME] doesn't exist"; exit 1; }
+	openvpn3 config-remove --config "$REMOVE_CONFIG_NAME"
+}
+
+connect() {
+	CONNECT_CONFIG_NAME="$1"
+	CONFIG_LIST=($(list | tail -n +3 | head -n -1 | awk '{print $1}'))
+	[ -z "${CONNECT_CONFIG_NAME}" ] && CONNECT_CONFIG_NAME="${CONFIG_LIST[0]}" || { grep "\-\-\-$CONNECT_CONFIG_NAME---" <<< $(echo -n "---";printf "%s---" "${CONFIG_LIST[@]}") > /dev/null || { echo "The connection [$CONNECT_CONFIG_NAME] doesn't exist"; exit 1; }; }
+	
+	openvpn3 session-start --config "${CONNECT_CONFIG_NAME}"
+}
+
+disconnect() {
+	DISCONNECT_CONFIG_NAME="$1"
+	SESSION_LIST=($(list s | grep "Config name:" | sed 's|Config name:||' | sed 's|^[[:space:]]*||'))
+	[ -z "${DISCONNECT_CONFIG_NAME}" ] && DISCONNECT_CONFIG_NAME="${SESSION_LIST[0]}" || { grep "\-\-\-$DISCONNECT_CONFIG_NAME---" <<< $(echo -n "---";printf "%s---" "${SESSION_LIST[@]}") > /dev/null || { echo "The connection [$DISCONNECT_CONFIG_NAME] doesn't have an active session"; exit 1; }; }
+
+	openvpn3 session-manage --disconnect --config "${DISCONNECT_CONFIG_NAME}"
+}
+
+stats() {
+	STATS_CONFIG_NAME="$1"
+	SESSION_LIST=($(list s | grep "Config name:" | sed 's|Config name:||' | sed 's|^[[:space:]]*||'))
+	[ -z "${STATS_CONFIG_NAME}" ] && STATS_CONFIG_NAME="${SESSION_LIST[0]}" || { grep "\-\-\-$STATS_CONFIG_NAME---" <<< $(echo -n "---";printf "%s---" "${SESSION_LIST[@]}") > /dev/null || { echo "The connection [$STATS_CONFIG_NAME] doesn't have an active session"; exit 1; }; }
+
+	openvpn3 session-stats --config "${STATS_CONFIG_NAME}"
+}
+
+log() {
+
+	OPTION="$1"
+	[ "$OPTION" = "all" ] && { sudo openvpn3-admin journal; exit 0; }
+
+	LOG_FILE=$(mktemp)
+	trap 'rm "$LOG_FILE"' EXIT
+	(
+		while true; do
+			sudo openvpn3-admin journal --since "$(date -d "1 second ago" +%Y-%m-%d\ %H:%M:%S)" 2> /dev/null >> $LOG_FILE
+			sleep 1
+		done
+	) &
+	LOOP_PID="$!"
+
+	trap 'kill "$LOOP_PID"' SIGINT
+	tail -n 0 -f $LOG_FILE
+}
+
+main() {
+	#binary check
+	command -v openvpn3 > /dev/null || { echo "openvpn3 command can't be found"; exit 1;}
+
+	ACTION="$1"
+	shift
+
+	case "$ACTION" in
+		import)
+			import "$@"
+			;;
+		list|ls)
+			list "$@"
+			;;
+		remove|rm)
+			remove "$@"
+			;;
+		c|connect|start)
+			connect "$@"
+			;;
+		d|disconnect|stop)
+			disconnect "$@"
+			;;
+		stats)
+			stats "$@"
+			;;
+		log)
+			log "$@"
+			;;
+		*)
+			echo "Invalid Input"
+			;;
+	esac
+
+}
+
+main "$@"
